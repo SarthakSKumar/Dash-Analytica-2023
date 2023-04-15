2740c5c5c8ee3a06c1ddd6e14d10e94efba65bf9
Sarthak S Kumar <sskworld9742@gmail.com>
2023-04-16 02:13:19 +0530

    Template Import, Python Scripts Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Map from "./components/map";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-export default function App() {
+import "./css/style.css";
+
+import "./charts/ChartjsConfig";
+
+// Import pages
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
+
   return (
-    <div>
-      <Map />
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
+
+export default App;
