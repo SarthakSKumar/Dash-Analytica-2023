953819b89eb66582b7baf8d4deca782cc7b9d78b
aryan2090 <55028506+aryan2090@users.noreply.github.com>
2023-04-16 11:33:14 +0530

    Update

--- Maps/Scripts/geojson2kml.py
+++ Maps/Scripts/geojson2kml.py
@@ -1,13 +1,26 @@
-import geojson2kml
-import json
+import random
 
-# read in the GeoJSON file
-with open('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Scripts/modified_geojson_file.geojson') as f:
-    geojson_data = json.load(f)
+# Define a list of colors
+colors = ['#00BF63', '#7ED957', '#FFDE59', '#FFBD59', '#FF914D', '#FF5757', '#FF3131']
 
-# convert to KML with styles preserved
-kml_data = geojson2kml.convert(geojson_data, style_mode=geojson2kml.StyleMode.POLYGON)
+# Open the KML file and read its contents
+try:
+    with open('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Datasets/RR_MAL_AYA_BSouth_ORG_MAP.kml', 'r') as kml_file:
+        kml_contents = kml_file.read()
+except FileNotFoundError:
+    print("Error: file not found!")
+    exit()
 
-# write the KML file
-with open('final.kml', 'w') as f:
-    f.write(kml_data)
+# Replace all occurrences of "{colorValue}" with a random color from the list
+while '{colorValue}' in kml_contents:
+    kml_contents = kml_contents.replace('{colorValue}', random.choice(colors), 1)
+
+# Write the updated contents back to the KML file
+try:
+    with open('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Datasets/RR_MAL_AYA_BSouth_ORG_MAP.kml', 'w') as kml_file:
+        kml_file.write(kml_contents)
+except:
+    print("Error: unable to write to file!")
+    exit()
+
+print("File updated successfully!")
--- Maps/Scripts/geojsontoKML.py
+++ Maps/Scripts/geojsontoKML.py
@@ -1,26 +1,38 @@
-import json
 import simplekml
+import json
 
-# Load GeoJSON file as a Python dictionary
-with open('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Datasets/final.geojson', 'r') as f:
+# Load GeoJSON file
+with open('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Datasets/precise.geojson') as f:
     data = json.load(f)
 
-# Create a simplekml object and set the styles
+# Create KML object
 kml = simplekml.Kml()
-for feature in data['features']:
-    if feature['geometry']['type'] == 'Point':
-        style = simplekml.Style()
-        style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/paddle/red-circle.png'
-        kml.newpoint(name=feature['properties']['name'], coords=[(feature['geometry']['coordinates'][0], feature['geometry']['coordinates'][1])], style=style)
-    elif feature['geometry']['type'] == 'LineString':
-        style = simplekml.Style()
-        style.linestyle.color = simplekml.Color.red
-        kml.newlinestring(name=feature['properties']['name'], coords=feature['geometry']['coordinates'], style=style)
-    elif feature['geometry']['type'] == 'Polygon':
-        style = simplekml.Style()
-        style.linestyle.color = simplekml.Color.red
-        style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.blue)
-        kml.newpolygon(name=feature['properties']['name'], outerboundaryis=feature['geometry']['coordinates'][0], innerboundaryis=feature['geometry']['coordinates'][1:], style=style)
 
-# Save the KML file
-kml.save('/Users/onepiece/Documents/Projects/Electhon-2023/Maps/Datasets/final.kml')
+# Loop over features and create Placemark for each feature
+for feature in data['features']:
+    # Get coordinates of feature
+    coords = feature['geometry']['coordinates'][0]
+
+    # Create LinearRing object from coordinates
+    lr = kml.newlinestring(name='')
+
+    # Set coordinates for LinearRing
+    lr.coords = coords
+
+    # Set style for LinearRing
+    lr.style.linestyle.width = 2
+    lr.style.linestyle.color = simplekml.Color.red
+    lr.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
+
+    # Try to get 'name' property from feature, if it exists
+    try:
+        name = feature['properties']['name']
+    except KeyError:
+        name = ''
+
+    # Create Placemark with LinearRing and name
+    placemark = kml.newpoint(name=name)
+    placemark.geometry = lr
+
+# Save KML file
+kml.save('output.kml')
